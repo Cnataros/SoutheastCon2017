b7f5d95d3b8f1b6e147da3e6a62f4b0423281ca9
Chris Nataros <cnataros@gmail.com>
2017-02-14 21:57:55 -0500

    update files to match current

--- RelayBoard.cpp
+++ RelayBoard.cpp
@@ -3,22 +3,25 @@
 
 RelayBoard::RelayBoard() {}
 
-RelayBoard::RelayBoard(uint8_t a, uint8_t b, uint8_t c, uint8_t d, uint8_t e)
+RelayBoard::RelayBoard(uint8_t a, uint8_t b, uint8_t c, uint8_t d, uint8_t e,uint8_t f)
 {
     pinMode(a, OUTPUT);
     pinMode(b, OUTPUT);
     pinMode(c, OUTPUT);
     pinMode(d, OUTPUT);
     pinMode(e, OUTPUT);
+    pinMode(f, OUTPUT);
     this->pin[0] = a;
     this->pin[1] = b;
     this->pin[2] = c;
     this->pin[3] = d;
     this->pin[4] = e;
+    this->polaritypin = f;
 
     for (int i = 0; i < PINS; i++) {
         digitalWrite(this->pin[i], HIGH);
     }
+    digitalWrite(this->polaritypin, HIGH);
 }
 
 RelayBoard::~RelayBoard() {}
@@ -31,3 +34,9 @@ void RelayBoard::select(uint8_t p)
     }
     digitalWrite(this->pin[p], LOW);
 }
+
+void RelayBoard::switchpolarity()
+{
+  digitalWrite(this->polaritypin, !digitalRead(this->polaritypin));
+}
+
--- RelayBoard.h
+++ RelayBoard.h
@@ -4,11 +4,13 @@
 class RelayBoard {
 private:
     uint8_t pin[5];
+    uint8_t polaritypin;
 public:
     RelayBoard();
-    RelayBoard(uint8_t, uint8_t, uint8_t, uint8_t, uint8_t);
+    RelayBoard(uint8_t,uint8_t, uint8_t, uint8_t, uint8_t, uint8_t);
     ~RelayBoard();
     void select(uint8_t);
+    void switchpolarity();
     static const uint8_t PINS = 5;
 };
 
